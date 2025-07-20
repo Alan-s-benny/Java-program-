e2c5ef83823d84b43b47adf7eab6bd937bec8c3f
Alan-s-benny <alansbenny2028@cs.sjcetpalai.ac.in>
2025-07-20 20:44:41 +0530

    Delete Factorial.java

--- Factorial.java
+++
@@ -1,9 +0,0 @@
-package mod1;
-public class Evennumber {
-    public static void main(String[] args) {
-        System.out.println("Even numbers from 1 to 100:");
-        for (int i = 2; i <= 100; i += 2) {
-            System.out.print(i + " ");
-        }
-    }
-}
