ae32a77bb443a10ea85d1011f3b700460ef6d3d7
Alan-s-benny <alansbenny2028@cs.sjcetpalai.ac.in>
2025-07-20 20:49:40 +0530

    Create Reverse a no.java

---
+++ Reverse a no.java
@@ -0,0 +1,18 @@
+package mod1;
+import java.util.Scanner;
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num;
+        System.out.print("Enter number to Reverse: ");
+        num = sc.nextInt();
+        int reverse = 0;
+
+        for (; num != 0; num /= 10) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+        }
+
+        System.out.println("Reversed number: " + reverse);
+    }
+}
